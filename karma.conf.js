6822669c997a44f9cf8cad0aeacf1a9da9550dff
Yuan Zhang <zhangyuan@users.noreply.github.com>
2015-10-22 21:24:12 +0800

    add reporter for karma

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'html'],
+
+    htmlReporter: {
+        outputDir: 'reporters/karma'
+    },
 
 
     // web server port
